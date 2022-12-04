268fa53aa7c5771a0cd9eb0775a2b38f5b22ec2e
Antonio Galindo <galindosvq@gmail.com>
2022-12-04 10:28:06 +0100

    Add day 3

--- index.ts
+++ index.ts
@@ -1,6 +1,6 @@
 import { answerPartOneDay1, answerPartTwoDay1 } from "./day1";
 import { answerPartOneDay2, answerPartTwoDay2 } from "./day2";
-import { answerPartOneDay3 } from "./day3";
+import { answerPartOneDay3, answerPartTwoDay3 } from "./day3";
 
 // DAY 1
 console.group("Day 1");
@@ -16,8 +16,9 @@ console.log("Answer Part 2", answerPartTwoDay2);
 console.groupEnd();
 console.log();
 
-// DAY 2
+// DAY 3
 console.group("Day 3");
 console.log("Answer Part 1", answerPartOneDay3);
+console.log("Answer Part 2", answerPartTwoDay3);
 console.groupEnd();
 console.log();
